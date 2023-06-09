6930ec4a5e2134e4229cde7b2ae42a0a65e4fadc
kaushaltheeG <kaushalsk09@gmail.com>
2023-06-09 11:30:59 -0700

    XCTest target bundle

--- UnitTesting/ViewModels/UnitTestViewModel.swift
+++ UnitTesting/ViewModels/UnitTestViewModel.swift
@@ -12,6 +12,7 @@ class UnitTestViewModel: ObservableObject {
     
     @Published var isPremium: Bool
     
+    // injection of dependency -> allows us to test the different cases of isPremium
     init(isPremium: Bool) {
         self.isPremium = isPremium
     }
